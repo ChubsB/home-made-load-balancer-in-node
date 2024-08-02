fbfbe9ebfaf39aae2b46c4cd3fdce2ed54fc58c9
hussain <hussainimam01@gmail.com>
2024-08-03 01:18:28 +0500

    basic working http server

--- lb.js
+++ lb.js
@@ -1,41 +1,14 @@
-const http = require("http");
-const { hostname } = require("os");
-const url = require("url");
+const http = require('http')
 
-const PORT = 80;
-const TARGET_SERVER = "http://example.com";
+const hostname = '127.0.0.1';
+const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(`Received request from ${req?.connection?.remoteAddress}`);
-  console.log(`${req?.method} ${req.url}`);
-  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello, World!\n')
+})
 
-  const options = {
-    hostname: url.parse(TARGET_SERVER).hostname,
-    port: 80,
-    path: req?.url,
-    method: req?.method,
-    headers: req.headers,
-  };
-
-  const proxy = http.request(options, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
-    proxyRes.pipe(res, {
-      end: true,
-    });
-  });
-
-  req.pipe(proxy, {
-    end: true,
-  });
-
-  proxy.on("error", (err) => {
-    console.log(`Error with proxy request: ${err.message}`);
-    res.writeHead(500);
-    res.end("Internal Server Error");
-  });
-});
-
-server.listen(PORT, () => {
-  console.log(`Load balancer running on port ${PORT}`);
-});
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
